2c54f37b8ba095f447e2311abc90779edb255e11
victor2996 <vicctor96@ukr.net>
2021-01-31 13:20:49 +0200

    landing finished

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (pageYOffset > 100) {
+            $('.navbar').addClass('navbar-active');
+            $('.navbar__phone').addClass('navbar__phone-active');
+            $('.container-adaptive').removeClass('container');
+        } else {
+            $('.navbar').removeClass('navbar-active');
+            $('.navbar__phone').removeClass('navbar__phone-active');
+            $('.container-adaptive').addClass('container');
+        }
+    });
+});
